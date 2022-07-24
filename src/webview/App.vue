3a3b5f4263c278c94042d938b7e96429c2485f87
Joe <soboleskijoe@gmail.com>
2022-07-24 11:11:40 -0700

    feat: made graphs collapsable

--- src/webview/App.vue
+++ src/webview/App.vue
@@ -1,70 +1,84 @@
 <template>
-  <h1>CodeGraphy</h1>
-  <div id="cy" ref="cyElement"></div>
+  <div style="display: flex; flex-direction: column; align-items: flex-start">
+    <h1>CodeGraphy</h1>
 
-  <h3>Full Graph</h3>
+    <button @click="toggleMainGraph()" class="hover-button">
+      <h3>Full Graph</h3>
+    </button>
+    <div ref="mainGraph">
+      <div id="cy" ref="cyElement"></div>
 
-  <button
-    id="reload"
-    style="width: 100%; margin-bottom: 8px"
-    @click="reload(mainCy, sortingOption)"
-  >
-    Reload
-  </button>
+      <button
+        id="reload"
+        style="width: 100%; margin-bottom: 8px"
+        @click="reload(mainCy, sortingOption)"
+      >
+        Reload
+      </button>
 
-  <div style="display: flex; flex-direction: column; margin-bottom: 8px">
-    <div>
-      <label>Sorting:</label>
-      <select style="margin-left: 8px" id="sorting-options">
-        <option value="cose" selected>Cose</option>
-        <option value="fcose">FCose</option>
-        <option value="cose-bilkent">Cose Bilkent</option>
-        <option value="cola">Cola</option>
-        <option value="grid">Grid</option>
-        <option value="random">Random</option>
-        <option value="circle">Circle</option>
-        <option value="concentric">Concentric</option>
-        <option value="breadthfirst">Breadthfirst</option>
-      </select>
+      <div style="display: flex; flex-direction: column; margin-bottom: 8px">
+        <div>
+          <label>Sorting:</label>
+          <select style="margin-left: 8px" id="sorting-options">
+            <option value="cose" selected>Cose</option>
+            <option value="fcose">FCose</option>
+            <option value="cose-bilkent">Cose Bilkent</option>
+            <option value="cola">Cola</option>
+            <option value="grid">Grid</option>
+            <option value="random">Random</option>
+            <option value="circle">Circle</option>
+            <option value="concentric">Concentric</option>
+            <option value="breadthfirst">Breadthfirst</option>
+          </select>
+        </div>
+
+        <div style="margin-top: 8px; display: flex; align-items: center">
+          <label>Hover:</label>
+          <label class="switch">
+            <input
+              type="checkbox"
+              id="hover-switch"
+              :checked="canUseHover"
+              @click="toggleHover()"
+            />
+            <span class="slider round"></span>
+          </label>
+        </div>
+
+        <div style="margin-top: 8px; display: flex; align-items: center">
+          <label>Hide Labels:</label>
+          <label class="switch">
+            <input
+              type="checkbox"
+              id="label-switch"
+              :checked="canUseLabels"
+              @click="toggleLabels()"
+            />
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
     </div>
 
-    <div style="margin-top: 8px; display: flex; align-items: center">
-      <label>Hover:</label>
-      <label class="switch">
+    <button
+      @click="toggleRelativeGraph()"
+      class="hover-button"
+      style="margin-top: 32px"
+    >
+      <h3>Local Graph</h3>
+    </button>
+    <div ref="relativeGraph">
+      <div id="cy-relative" ref="cyElementRelative"></div>
+
+      <div style="margin-top: 8px">
+        <label>Local Depth:</label>
         <input
-          type="checkbox"
-          id="hover-switch"
-          :checked="canUseHover"
-          @click="toggleHover()"
+          style="margin-left: 8px"
+          id="local-depth"
+          v-model="localDepth"
+          @change="depthChange"
         />
-        <span class="slider round"></span>
-      </label>
-    </div>
-
-    <div style="margin-top: 8px; display: flex; align-items: center">
-      <label>Hide Labels:</label>
-      <label class="switch">
-        <input
-          type="checkbox"
-          id="label-switch"
-          :checked="canUseLabels"
-          @click="toggleLabels()"
-        />
-        <span class="slider round"></span>
-      </label>
-    </div>
-
-    <h3 style="margin-top: 32px">Local Graph</h3>
-    <div id="cy-relative" ref="cyElementRelative"></div>
-
-    <div style="margin-top: 8px">
-      <label>Local Depth:</label>
-      <input
-        style="margin-left: 8px"
-        id="local-depth"
-        v-model="localDepth"
-        @change="depthChange"
-      />
+      </div>
     </div>
   </div>
 </template>
@@ -88,6 +102,11 @@ const nodeCurrentFile = currentFile;
 
 const cyElement: Ref<HTMLElement | undefined> = ref();
 const cyElementRelative: Ref<HTMLElement | undefined> = ref();
+const mainGraph: Ref<HTMLElement | undefined> = ref();
+const relativeGraph: Ref<HTMLElement | undefined> = ref();
+
+let displayMainGraph = true;
+let displayRelativeGraph = true;
 
 let mainCy: any = null;
 let relativeCy: any = null;
@@ -137,5 +156,29 @@ const depthChange = () => {
   relativeCy.elements().remove();
   relativeCy.add(nodes);
   reload(relativeCy, "reload");
+};
+
+const toggleMainGraph = () => {
+  displayMainGraph = !displayMainGraph;
+
+  if (mainGraph.value) {
+    if (displayMainGraph) {
+      mainGraph.value.style.display = "block";
+    } else {
+      mainGraph.value.style.display = "none";
+    }
+  }
+};
+
+const toggleRelativeGraph = () => {
+  displayRelativeGraph = !displayRelativeGraph;
+
+  if (relativeGraph.value) {
+    if (displayRelativeGraph) {
+      relativeGraph.value.style.display = "block";
+    } else {
+      relativeGraph.value.style.display = "none";
+    }
+  }
 };
 </script>
