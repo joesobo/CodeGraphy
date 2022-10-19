742c85e7afd3357f2b140d0fe82a81ccb05f229d
Joe Soboleski <soboleskijoe@gmail.com>
2022-10-19 04:28:28 -0700

    d3 test

--- src/webview/App.vue
+++ src/webview/App.vue
@@ -2,6 +2,12 @@
 	<div class="flex flex-col flex-start mb-4">
 		<h1>CodeGraphy</h1>
 
+		<Disclosure title="D3" open>
+			<svg id="graph" width="600" height="600">
+				<g transform="translate(50, 0)"></g>
+			</svg>
+		</Disclosure>
+
 		<Disclosure title="Full Graph" open>
 			<div
 				id="cy"
@@ -106,6 +112,8 @@ import {
 import NodeMetaController from "./view/NodeMetaController.vue"
 import Disclosure from "./components/Disclosure.vue"
 
+import * as d3 from "d3"
+
 let nodeFiles = files
 let nodeConnections = connections
 let nodeCurrentFile = currentFile
@@ -164,5 +172,57 @@ const refreshGraphs = () => {
 		nodeConnections,
 		localDepth
 	)
+}
+
+// test
+var width = 600,
+	height = 400
+
+var colorScale = ["orange", "lightblue", "#B19CD9"]
+var xCenter = [100, 300, 500]
+
+var numNodes = 100
+var nodes = d3.range(numNodes).map(function (d, i) {
+	return {
+		radius: Math.random() * 25,
+		category: i % 3
+	}
+})
+
+var simulation = d3
+	.forceSimulation(nodes)
+	.force("charge", d3.forceManyBody().strength(5))
+	.force(
+		"x",
+		d3.forceX().x(function (d) {
+			return xCenter[d.category]
+		})
+	)
+	.force(
+		"collision",
+		d3.forceCollide().radius(function (d) {
+			return d.radius
+		})
+	)
+	.on("tick", ticked)
+
+function ticked() {
+	var u = d3
+		.select("#graph")
+		.selectAll("circle")
+		.data(nodes)
+		.join("circle")
+		.attr("r", function (d) {
+			return d.radius
+		})
+		.style("fill", function (d) {
+			return colorScale[d.category]
+		})
+		.attr("cx", function (d) {
+			return d.x
+		})
+		.attr("cy", function (d) {
+			return d.y + 300
+		})
 }
 </script>
